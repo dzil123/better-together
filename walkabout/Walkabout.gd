30e556bdaca5b3e8c75c7f09f526829c2e1ba401
ArtlessAvian <artlessavian@gmail.com>
2022-02-11 23:00:17 -0800

    walking about, room movement

--- walkabout/Walkabout.gd
+++ walkabout/Walkabout.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
 var day = 0
-var entrance = 0
 var roomId = 0
 
 var timer = 0
 var can_reset_timer = true
 
 export (Array, PackedScene) var roomIdToScene = [];
+
+func _ready():
+	goto_room(0, 0)
 
 func _physics_process(delta):
 	if roomId != 0:
@@ -19,11 +21,12 @@ func _physics_process(delta):
 		$Room.emit_signal("lose")
 		pass
 
-func goto_room(id):
+func goto_room(id, entrance):
 	roomId = id
 	var roomPacked : PackedScene = roomIdToScene[roomId]
 	remove_child($Room)
 
 	var newRoom = roomPacked.instance()
 	newRoom.name = "Room" # eh
+	newRoom.get_node("Player").entrance = entrance;
 	add_child(newRoom)
---
+++ walkabout/elements/Player.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var movable = true
+var entrance = 0
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_up"):
+		var areas = $Area2D.get_overlapping_areas()
+		for area in areas:
+			if area.name == "PortalBox":
+				get_tree().get_root().get_node("Walkabout").goto_room(area.get_parent().goto_room, area.get_parent().goto_entrance)
+				return
+
+	var vx = 0
+	if Input.is_action_pressed("ui_left"):
+		vx -= 500
+	if Input.is_action_pressed("ui_right"):
+		vx += 500
+	position.x += vx * delta
