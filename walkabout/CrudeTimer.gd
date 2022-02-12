615a49d2ec1c53efebaaf82038fad5688576c972
ArtlessAvian <artlessavian@gmail.com>
2022-02-12 13:13:35 -0800

    forgot to commit last night, but a better timer and background colors

--- walkabout/CrudeTimer.gd
+++ walkabout/CrudeTimer.gd
@@ -8,8 +8,13 @@ func get_walkabout():
 
 func _process(delta):
 	var timer_in_ms = int(get_walkabout().timer * 1000)
-	var ms = timer_in_ms % 1000
-	var secs = int(timer_in_ms / 1000) % 60
-	var mins = int(timer_in_ms / 60000)
+	var ms = str(timer_in_ms % 1000)
+	var secs = str(int(timer_in_ms / 1000) % 60)
+	var mins = str(int(timer_in_ms / 60000))
 
-	self.text = str(mins) + " : " + str(secs) + " : " + str(ms)
+	if len(secs) == 1:
+		secs = "0" + secs
+	while len(ms) < 3:
+		ms = "0" + ms
+
+	self.text = mins + " : " + secs + " : " + ms
