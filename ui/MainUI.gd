6b7521690da28184c9a522db53872dc7247b7942
dzil123 <5725958+dzil123@users.noreply.github.com>
2022-02-12 19:12:49 -0800

    main menu

--- ui/MainUI.gd
+++ ui/MainUI.gd
@@ -6,6 +6,7 @@ export(NodePath) var timer_path
 
 var commands = {}
 var block_progress = false
+var in_dialog = false
 
 onready var timer_label = get_node(timer_label_path) as Label
 onready var options_list = get_node(option_list_path)
@@ -14,6 +15,7 @@ onready var yarn = $YarnStory
 onready var topbox = $TopBox
 
 signal movement_enabled(is_enabled)
+
 
 func _init():
 	pass
@@ -42,11 +44,13 @@ func _step_story(value = null):
 	if yarn.story_state == null:
 		yarn.current_function = ""
 		topbox.visible = false
+		in_dialog = false
 		emit_signal("movement_enabled", true)
 
 
 func _on_YarnStory_dialogue(yarn_node, actor, message):
 	topbox.visible = true
+	in_dialog = true
 	emit_signal("movement_enabled", false)
 
 
@@ -57,12 +61,14 @@ func _on_DialogText_Label_line_complete():
 
 func _on_YarnStory_options(yarn_node, options):
 	block_progress = true
+	in_dialog = true
 	emit_signal("movement_enabled", false)
 	options_list._on_YarnStory_options(yarn_node, options)
 
 
 func _on_select_option(option):
 	block_progress = false
+	in_dialog = true
 	emit_signal("movement_enabled", false)
 	_step_story(option)
 
--- walkabout/CrudeTimer.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-
-func get_walkabout():
-	var thing = self
-	while thing.name != "Walkabout":
-		thing = thing.get_parent()
-	return thing
